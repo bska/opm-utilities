e1ac6bbb604659c3ed7c43f8303576d23e309f72
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-02-17 15:42:15 +0100

    fix build with c++-20
    this header uses symbols before they are defined. reorder code
    so that does not happen (this is essentially the same fix that is
    done in dune 2.11)

--- opm-ci-builder/scripts/build_debug.sh
+++ opm-ci-builder/scripts/build_debug.sh
@@ -107,3 +107,4 @@ cd /debug_iter
 patch -p0 < /tmp/opm/patches/dune-common/0001-gpu_patch.patch
 patch -p0 < /tmp/opm/patches/dune-istl/0001-missing_initializers.patch
 patch -p0 < /tmp/opm/patches/dune-fem/0002-missing_include.patch
+patch -p0 < /tmp/opm/patches/dune-grid/0001-gpu_patch.patch
--- opm-ci-builder/scripts/patch_system.sh
+++ opm-ci-builder/scripts/patch_system.sh
@@ -6,8 +6,10 @@ cd /usr/include
 patch -p1 < /tmp/opm/patches/dune-common/0001-gpu_patch.patch
 patch -p1 < /tmp/opm/patches/dune-istl/0001-missing_initializers.patch
 patch -p1 < /tmp/opm/patches/dune-fem/0002-missing_include.patch
+patch -p1 < /tmp/opm/patches/dune-grid/0001-gpu_patch.patch
 
 cd /dune/serial
 patch -p0 < /tmp/opm/patches/dune-common/0001-gpu_patch.patch
 patch -p0 < /tmp/opm/patches/dune-istl/0001-missing_initializers.patch
 patch -p0 < /tmp/opm/patches/dune-fem/0002-missing_include.patch
+patch -p0 < /tmp/opm/patches/dune-grid/0001-gpu_patch.patch
